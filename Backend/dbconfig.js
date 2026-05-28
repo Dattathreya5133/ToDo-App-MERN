b1c19d5536ecb7121a233b9f26783e655eefe267
Dattathreya5133 <dattathreyapai15@gmail.com>
2026-05-28 10:04:32 +0530

    fixed mongodb password

--- Backend/dbconfig.js
+++ Backend/dbconfig.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const url = "mongodb://RamaKrishna:RamaKrishna151@ac-vedxjb4-shard-00-00.ul41zty.mongodb.net:27017,ac-vedxjb4-shard-00-01.ul41zty.mongodb.net:27017,ac-vedxjb4-shard-00-02.ul41zty.mongodb.net:27017/?ssl=true&replicaSet=atlas-12ef7q-shard-0&authSource=admin&appName=ToDo-MERN"
+const url = "mongodb+srv://RamaKrishna:RamaKrishna151%40@todo-mern.ul41zty.mongodb.net/?appName=ToDo-MERN"
 const dbName = 'Todo-mern';
 const collectionName = 'todolist'
 const client = new MongoClient(url)
